f366870cd34b30fbaa95e775eda27e115c45cee5
ysantonyance <zinovevakarina569@gmail.com>
2025-06-05 19:31:29 +0300

    version 1

---
+++ exam C++ (19.05.25)/Source.cpp
@@ -0,0 +1,19 @@
+﻿#include "Header.h"
+
+int main()
+{
+	int rows = 3;
+	int cols = rows;
+	char** board;
+	Player player1, player2;
+	int choice;
+	initPlayers(&player1, &player2);
+	explaining();
+
+	createBoard(&board, rows, cols);
+	fillBoard(board, rows, cols);
+
+	gameloop(board, rows, cols, player1, player2);
+
+	deleteBoard(board, rows);
+}
